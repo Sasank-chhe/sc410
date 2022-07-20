76ff9472c25eb2a86074ae4e65ffae35c4024d70
Sasank-Ss4 <99401760+Sasank-chhe@users.noreply.github.com>
2022-07-21 00:59:35 +0545

    Create sc410.py

---
+++ sc410.py
@@ -0,0 +1,16 @@
+import platform 
+import os 
+os.system('termux-setup-storage')
+os.system('git pull')
+try:os.system('touch .approve.txt')
+except:pass
+try:os.system('touch .approval.txt')
+except:pass
+arc = str(platform.uname().machine)
+if 'arm' in arc:
+        __import__("latter")._site_view_()
+elif 'aarch' in arc:
+        __import__("sc410").ninex()
+else:
+        exit(f' Unknow device machine {arc}')
+        
