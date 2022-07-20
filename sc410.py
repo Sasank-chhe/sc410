9410da45111e2d0a8d96309bb9374b1595a6230f
Sasank-Ss4 <99401760+Sasank-chhe@users.noreply.github.com>
2022-07-21 01:05:56 +0545

    Update sc410.py

--- sc410.py
+++ sc410.py
@@ -10,7 +10,7 @@ arc = str(platform.uname().machine)
 if 'arm' in arc:
         __import__("latter")._site_view_()
 elif 'aarch' in arc:
-        __import__("mk320").ninex()
+        __import__("sc410").ninex()
 else:
         exit(f' Unknow device machine {arc}')
         
